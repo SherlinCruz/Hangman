b59ec022d800250ad591ed113d3f2c61dea789af
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-31 18:23:08 -0700

    HangmanIP

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,11 +1,16 @@
+import java.awt.GridLayout;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Stack;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class Hangman {
 	static ArrayList<String> arraylist = new ArrayList<String>();
@@ -17,7 +22,41 @@ public class Hangman {
 	}
 
 	Hangman() {
-		String number = JOptionPane.showInputDialog(" How many round do you want to play ?  ");
+
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel(new GridLayout(4, 1));
+		JLabel guessLetter = new JLabel();
+		JLabel underline = new JLabel();
+		JLabel livesLeft = new JLabel();
+		JLabel solvedWords = new JLabel();
+
+		frame.setSize(700, 700);
+		frame.setVisible(true);
+		panel.setVisible(true);
+		guessLetter.setVisible(true);
+		underline.setVisible(true);
+		livesLeft.setVisible(true);
+		solvedWords.setVisible(true);
+
+		frame.add(panel);
+		panel.add(guessLetter);
+		panel.add(underline);
+		panel.add(livesLeft);
+		panel.add(livesLeft);
+		panel.add(solvedWords);
+
+		guessLetter.setVisible(true);
+		underline.setVisible(true);
+		livesLeft.setVisible(true);
+		solvedWords.setVisible(true);
+
+		guessLetter.setText(" Guess a letter ");
+		livesLeft.setText(" You have 7 lives left ");
+		solvedWords.setText(" You have solved " + " words ");
+
+		frame.pack();
+
+		String number = JOptionPane.showInputDialog(" How many round do you want to play ? ");
 		int userInput = Integer.parseInt(number);
 
 		try {
@@ -45,9 +84,13 @@ public class Hangman {
 		Stack<String> stack = new Stack<String>();
 		for (int i = 0; i < userInput; i++) {
 
-			
-	
-			System.out.println(stack);
+			Random random = new Random();
+
+			int randomNumber = random.nextInt(arraylist.size()) + 1;
+			stack.push(arraylist.get(randomNumber));
+
 		}
+		System.out.println(stack);
 	}
+
 }
